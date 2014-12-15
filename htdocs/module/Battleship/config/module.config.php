3cd45e920dbdf3b3f95e54a607ac7eee5a4560d2
Momchil Milev <momchil.milev@bfashion.com>
2014-12-15 17:42:30 +0200

    Init

--- htdocs/module/Battleship/config/module.config.php
+++ htdocs/module/Battleship/config/module.config.php
@@ -1,70 +1,27 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 return array(
     'router' => array(
         'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route' => '/',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
-                        'action' => 'index',
-                    ),
-                ),
-            ),
             'battleship-results' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route' => '/battleship',
+                    'route' => '/',
                     'defaults' => array(
                         'controller' => 'Battleship\Controller\Index',
                         'action' => 'index',
                     ),
                 ),
             ),
-            'play-battleship' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route' => '/battleship/play',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Battleship\Controller',
-                        'controller' => 'Index',
-                        'action' => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(),
-                        ),
-                    ),
-                ),
-            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
+            // using the path /battleship/:controller/:action
+            'play' => array(
                 'type' => 'Literal',
                 'options' => array(
-                    'route' => '/application',
+                    'route' => '/play',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
+                        '__NAMESPACE__' => 'Battleship\Controller',
                         'controller' => 'Index',
                         'action' => 'index',
                     ),
@@ -107,7 +64,7 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Battleship\Controller\Index' => 'Battleship\Controller\IndexController'
         ),
     ),
     'view_manager' => array(
@@ -118,7 +75,7 @@ return array(
         'exception_template' => 'error/index',
         'template_map' => array(
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'battleship/index/index' => __DIR__ . '/../view/battleship/index/index.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/index' => __DIR__ . '/../view/error/index.phtml',
         ),
@@ -134,15 +91,15 @@ return array(
     ),
     'doctrine' => array(
         'driver' => array(
-            'application_entities' => array(
+            'battleship_entities' => array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
                 'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/Application/Entity')
+                'paths' => array(__DIR__ . '/../src/Battleship/Entity')
             ),
 
             'orm_default' => array(
                 'drivers' => array(
-                    'Application\Entity' => 'application_entities'
+                    'Battleship\Entity' => 'battleship_entities'
                 )
             )
         )
