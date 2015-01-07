296bfc8920047008f76cdea544ac536271e2d838
Momchil Milev <momchil.milev@gmail.com>
2015-01-07 18:10:00 +0200

    Debug bar

--- htdocs/config/application.config.php
+++ htdocs/config/application.config.php
@@ -2,11 +2,11 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'ZendDeveloperTools',
         'DoctrineModule',
         'DoctrineORMModule',
         'Application',
         'Battleship',
+        'ZendDeveloperTools',
 //        'ZfTable',
 //        'ZfSnapPhpDebugBar'
     ),
